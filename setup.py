88e14d5bc78b775303452e1722c10d2d0ddc342a
Anton Patrushev <apatrushev@gmail.com>
2017-08-22 17:04:47 +0500

    setup cleanup. license.

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 setup(
     name='get-venv-builder',
     version='0.1',
+    url='https://github.com/apatrushev/get-venv-builder',
+    description='Tool to create scripts for virtualenv bootstrap.',
+    license='MIT',
+
     author='Anton Patrushev',
     author_email='apatrushev@gmail.com',
-    description='Tool to create virtualenv bootstrap scripts.',
-    zip_safe=False,
+
     packages=[
         'get_venv_builder',
     ],
@@ -15,10 +18,22 @@ setup(
             'resources/*'
         ]
     },
+    zip_safe=False,
+    platforms='any',
+
     install_requires=[
         'click',
     ],
     entry_points={
         'console_scripts': ['gvb=get_venv_builder.build:main'],
-    }
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ]
 )
